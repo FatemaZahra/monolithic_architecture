8d3fb7ec95de40a3435b4a900efb1ac41665a159
Fatema Zahra <fatema.zahra1312@gmail.com>
2022-08-17 09:50:39 +0100

    reverse proxy

--- provision.sh
+++ provision.sh
@@ -12,7 +12,7 @@ sudo apt-get install nginx -y
 
 # enable nginx
 
-sudo systemctl enable ngnix
+sudo systemctl enable nginx
 sudo systemctl start nginx
 
 
@@ -21,7 +21,7 @@ sudo systemctl start nginx
 sudo apt-get install nodejs -y
 
 # install version 6
-
+# sudo apt-get install npm -y
 sudo apt-get purge nodejs npm
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
@@ -41,6 +41,12 @@ sudo apt-get update -y
 #upgrade
 
 sudo apt-get upgrade -y
+
+#reverse_proxy
+
+sudo cp -f app/app/proxy_file /etc/nginx/sites-available/default
+# sudo nginx -t
+sudo systemctl restart nginx
 
 
 
