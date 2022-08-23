ed40be68f5ea2788d71962196e6d23e30e9d9f00
Fatema Zahra <102330725+FatemaZahra@users.noreply.github.com>
2022-08-23 10:26:36 +0100

    Update provision.sh

--- environment/provision.sh
+++ environment/provision.sh
@@ -6,3 +6,29 @@ sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable
 sudo apt-get install python -y
+
+# install nodejs
+
+sudo apt-get install nodejs -y
+
+# install version 6
+# sudo apt-get install npm -y
+sudo apt-get purge nodejs npm
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# install node.js again
+
+sudo apt-get install nodejs -y
+
+# install pm2
+
+sudo npm install pm2 -g
+
+# update
+
+sudo apt-get update -y
+
+#upgrade
+
+sudo apt-get upgrade -y
